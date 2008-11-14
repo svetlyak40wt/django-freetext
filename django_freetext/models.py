674d0fd14537087b96abbde1f64015af4549df47
Alexander Artemenko <svetlyak.40wt@gmail.com>
2008-11-14 20:19:27 +0300

    Russian translation.

--- django_freetext/models.py
+++ django_freetext/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 class FreeText(models.Model):
     """
@@ -7,13 +8,15 @@ class FreeText(models.Model):
     any template with the use of a special template
     tag
     """
-    key = models.CharField('key', help_text="A unique name for this FreeText of content", blank=False, max_length=255, unique=True)
-    content = models.TextField('content', blank=True)
-    active = models.BooleanField("active", default=False)
+    key = models.CharField(_('key'),
+                           help_text=_('A unique name for this FreeText of content'),
+                           blank=False, max_length=255, unique=True)
+    content = models.TextField(_('content'), blank=True)
+    active = models.BooleanField(_('active'), default=False)
 
     class Meta:
-        verbose_name = 'free text'
-        verbose_name_plural = 'free texts'
+        verbose_name = _('free text')
+        verbose_name_plural = _('free texts')
 
     def __unicode__(self):
         return u"%s" % (self.key,)
